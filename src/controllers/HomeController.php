1ddec4dfe17078fc09cde24826b75535c39ec3ae
Beau <beaulm@gmail.com>
2014-07-31 11:46:56 -0500

    Cleaned up use requirments

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,23 +1,14 @@
 <?php namespace Thirdsteplabs\Apibase;
 
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Lang;
-use Symfony\Component\HttpFoundation\File\File as SFile;
-use Illuminate\Support\Facades\Validator as LValidator;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Routing\Controllers\Controller;
 
 class HomeController extends \BaseController {
-	
+
 	/*
 	|--------------------------------------------------------------------------
 	| Default Home Controller
--- src/models/User.php
+++ src/models/User.php
@@ -1,5 +1,6 @@
 <?php namespace Thirdsteplabs\Apibase;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
