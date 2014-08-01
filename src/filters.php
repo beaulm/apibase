250e38189734485b9da97c63732470be271807f2
Beau <beaulm@gmail.com>
2014-08-01 12:00:39 -0500

    Moved all routes into the 'before' app filter so they don't take presedence over user routes

--- src/filters.php
+++ src/filters.php
@@ -13,7 +13,38 @@
 
 App::before(function($request)
 {
-	//
+	Route::get('/', array('uses' => 'HomeController@home', 'as' => 'home'));
+
+	Route::get('login', array('uses' => 'HomeController@loginform', 'as' => 'login'));
+
+	Route::post('login', array('uses' => 'HomeController@login'));
+
+	Route::get('logout', array('uses' => 'HomeController@logout', 'as' => 'logout'));
+
+	Route::get('password/forgot', array('uses' => 'HomeController@forgotpasswordform'));
+
+	Route::get('password/remind', array('uses' => 'HomeController@remindpasswordform'));
+
+	Route::post('password/remind', array('uses' => 'HomeController@remindpassword', 'as' => 'remind'));
+
+	Route::get('password/reset/{token}', array('uses' => 'HomeController@resetpasswordform'));
+
+	Route::post('password/reset/{token}', array('uses' => 'HomeController@resetpassword', 'as' => 'reset'));
+
+	Route::any('api/v1/login', array('as' => 'apilogin', 'uses' => 'ApiController@login'));
+	Route::group(array('before' => 'apiauth'), function()
+	{
+	    //Route::controller('api/v1', 'ApiController');
+		Route::any('api/v1/logout', array('uses' => 'ApiController@anyLogout', 'as' => 'anyLogout'));
+		Route::group(array('before' => 'makeSureModelExists'), function()
+		{
+			Route::get('api/v1/{modelName}', array('uses' => 'ApiController@getAll', 'as' => 'getAll'));
+			Route::get('api/v1/{modelName}/{id}', array('uses' => 'ApiController@getSpecific', 'as' => 'getSpecific'));
+			Route::post('api/v1/{modelName}', array('uses' => 'ApiController@createOne', 'as' => 'createOne'));
+			Route::put('api/v1/{modelName}/{id}', array('uses' => 'ApiController@editOne', 'as' => 'editOne'));
+			Route::delete('api/v1/{modelName}/{id}', array('uses' => 'ApiController@deleteOne', 'as' => 'deleteOne'));
+		});
+	});
 });
 
 
@@ -79,7 +110,20 @@ Route::filter('makeSureModelExists', function($route)
 {
 	if(!class_exists(ucfirst($route->getParameter('modelName'))))
 	{
-		return Response::json(array('code' => 401, 'message' => Lang::get('apibase::thirdstep.response_message.invalid_request')), 401);
+		$validRoute = false;
+		$routeCollection = Route::getRoutes();
+		foreach($routeCollection as $route)
+		{
+			if(Request::path() == $route->getPath())
+			{
+				$validRoute = true;
+				break;
+			}
+		}
+		if(!$validRoute)
+		{
+			return Response::json(array('code' => 401, 'message' => Lang::get('apibase::thirdstep.response_message.invalid_request')), 401);
+		}
 	}
 });
 
