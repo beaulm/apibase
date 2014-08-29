7337f8cb4151f5eacefbd7ea77fc315478a8b55e
Beau <beaulm@gmail.com>
2014-08-29 07:48:52 -0500

    Updated master layout

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -27,7 +27,7 @@
 				@yield('content')
 			</div>
 		</div>
-		<script type="text/javascript" src="/js/jquery-1.9.1.min.js"></script>
+		<script type="text/javascript" src="/js/jquery-2.1.1.min.js"></script>
 		@yield('footerScripts')
 	</body>
 </html>
